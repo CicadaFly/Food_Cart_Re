7b0ca5922d6cb1fd3d16caa43249aefa32ac717e
CicadaFly <110823334+CicadaFly@users.noreply.github.com>
2022-11-03 21:12:52 +0800

    11/3

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, {Fragment} from 'react'
+import classes from './Header.module.css'
+import mealsimage from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
+const Header = props =>{
+
+return(
+<Fragment>
+  <header className={classes.header}>
+  <h1>ReactMealsAPP</h1>
+  <HeaderCartButton />
+  </header>
+  <div className={classes['main-image']}>
+    <img src={mealsimage} alt="background"/>
+  </div>
+</Fragment>
+)
+}
+
+export default Header
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,18 @@
+//import React,{useContext, useEffect, useState} from 'react'
+import classes from './HeaderCartButton.module.css'
+import CartIcon from '../Cart/CartIcon'
+//import CartContext from '../../store/cart-context'
+
+const HeaderCartButton = (props) => {
+  return (
+  <button className={classes.button}>
+    <span className={classes.icon}>
+      <CartIcon/>
+    </span>
+    <span>購物車</span>
+    <span className={classes.badge}>5</span>
+  </button>
+  );
+};
+
+export default HeaderCartButton
