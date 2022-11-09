08a0b65a8ecdacc392a2cc079926b8413514c4de
CicadaFly <110823334+CicadaFly@users.noreply.github.com>
2022-11-09 18:48:43 +0800

    http request

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,34 +1,43 @@
 import classes from './AvailableMeals.module.css'
 import Card from '../UI/Card'
 import MealsItem from './MealsItem/MealsItem';
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    { 
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    { 
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+import { useEffect, useState } from 'react';
+// const DUMMY_MEALS = [
+//     {
+//       id: 'm1',
+//       name: 'Sushi',
+//       description: 'Finest fish and veggies',
+//       price: 22.99,
+//     },
+//     { 
+//       id: 'm2',
+//       name: 'Schnitzel',
+//       description: 'A german specialty!',
+//       price: 16.5,
+//     },
+//     {
+//       id: 'm3',
+//       name: 'Barbecue Burger',
+//       description: 'American, raw, meaty',
+//       price: 12.99,
+//     },
+//     { 
+//       id: 'm4',
+//       name: 'Green Bowl',
+//       description: 'Healthy...and green...',
+//       price: 18.99,
+//     },
+//   ];
 
 const AvailableMeals =()=>{
+  useEffect(()=> {
+    const fetchMeals = async() => {
+      const response = await fetch('https://react-test-74361-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json');
+      const responseData = await response.json();
+    
+    }
+  }, [])   
+  fetch(input: "Meal_List" )
     const MealsList = DUMMY_MEALS.map((meal) =>(
         <MealsItem
             id = {meal.id} 
